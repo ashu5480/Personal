6573e3f14dc04ec732ab15399a7d56393d1eb801
Ashutosh <55346840+ashu5480@users.noreply.github.com>
2019-12-13 10:18:27 +0530

    New Copy-Constructor By me

---
+++ CopyConstructor.java
@@ -0,0 +1,39 @@
+//Copy Constructor Using Constructor
+package java_basics;
+
+class CopyConstructor1 {
+
+	//First Step Create Constructor
+	
+	//Second Step instance Variable
+	int id;
+	String Name;
+	
+	//Create Constructor
+	CopyConstructor1(int id,String Name)
+	{
+		this.id=id;
+		this.Name=Name;
+	}
+	
+	//Create CopyConstructor Or Second Constructor
+	CopyConstructor1(CopyConstructor1 copy1)
+	{
+		id= copy1.id;
+		Name=copy1.Name;
+	}
+	void display()
+	{
+		System.out.println("ID"+id+"//"+"Name"+Name);
+	}
+	
+}
+public class CopyConstructor
+{
+	public static void main(String[] args) {
+		CopyConstructor1 copy = new CopyConstructor1(1,"ASHU");
+		CopyConstructor1 copy1 = new CopyConstructor1(copy);
+		copy.display();
+		copy1.display();
+	}
+}
